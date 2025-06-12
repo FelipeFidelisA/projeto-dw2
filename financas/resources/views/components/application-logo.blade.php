369aab74c7b3209e560b16b48cf2fc5eb1e12e7f
alexandrendev <alxfn86@gmail.com>
2025-06-12 15:22:25 -0300

    fix: tamano da logo na navbar

--- financas/resources/views/components/application-logo.blade.php
+++ financas/resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <div class="flex items-center">
-    <img src="/img/icon-alvora.png" alt="Alvora" class="h-8 w-auto">
+    <img src="/img/icon-alvora.png" alt="Alvora" class="h-9 w-auto">
     <span class="ml-2 text-xl font-bold text-gray-900">Alvora</span>
 </div>
